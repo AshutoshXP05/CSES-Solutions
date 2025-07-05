dee23db3ff9edf2a4aef659a8da5ad23f5d55d5e
Ashutosh Kumar <pathakashutoshdmr@gmail.com>
2025-07-05 23:40:53 +0530

    Binary Search on Answer Technique

--- SORTING_SEARCHING/Factory_Machines.cpp
+++ SORTING_SEARCHING/Factory_Machines.cpp
@@ -1,55 +1,66 @@
+
+//  Binary Search on Answer Problem --->
+
 #include <bits/stdc++.h>
-//with test cases
 using namespace std;
-void solve();
-#define f(i,s,e) for(long long int i=s;i<e;i++)      // FOR LOOP 
-
-#define MOD 1000000007         // MODULUS 
-#define PI 3.1415926535897932384626433832795
-#define pb push_back       // PUSH _ BACK 
-#define fi first         // FIRST 
-#define se second    //SECOND 
-#define endl '\n'      //  endl se fast hota hai isly we use /N 
 typedef long long ll;
+void solve();
+#define MOD 1000000007 // MODULUS
+#define PI 3.1415926535897932384626433832795
+#define pb push_back // PUSH _ BACK
+#define fi first     // FIRST
+#define se second    // SECOND
+#define endl '\n'    //  endl se fast hota hai isly we use /N
 
-ll expo(ll a, ll b) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % MOD; a = (a * a) % MOD; b = b >> 1;} return res;} 
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+#define yes cout << "YES\n"; //  For yes
+#define no cout << "NO\n";   // For No
 
-
-// Modular Arithmetic
-ll mod_add(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
-
-ll mod_sub(ll a, ll b) { return ((a % MOD) - (b % MOD) + MOD) % MOD; }
-
-ll mod_mul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
-
-ll mod_inv(ll a) { return expo(a, MOD - 2); }  // Using Fermat's Little Theorem
-
-#define yes cout<<"YES\n";              //  For yes 
-#define no cout<<"NO\n";               // For No
-typedef vector<int> vi;                // vector of int 
-typedef vector<vi> vvi;                 // Vector of vector of int 
-typedef pair<int, int> ii;      // Pair of int int 
-typedef vector<ii> vii;        // Vector of pair of int int 
-typedef vector<ll> vll;     // Vector of long long 
-typedef double ld;         // For double
-#define mii map<int, int>  // Map of int int 
-#define si set<int>      // Set of integer 
-#define sc set<char>    // Set of Character 
-
-
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.precision(10);
     cout << fixed;
+    // long long t;          // Test cases wala ..
+    // cin>>t;
+    // while(t--){
     solve();
+    // }
     return 0;
 }
 
-void solve(){
-    long long n,i,j,k,count=0,ans;
-    cin>>n;
-    vector<long long> a(n);
-    // f(i,0,n)cin>>a[i];            
-    // cout<<count<<endl;
+ll cntProducts ( ll mid, vector<int> &arr) {
+    ll cnt = 0;
+    for ( auto it : arr ) {
+        cnt += (mid / it);
+    }
+    return cnt;
+}
+
+void solve()
+{
+    ll n, t;
+    cin >> n >> t;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    ll low = *min_element(arr.begin(), arr.end());
+    ll high = low * t;
+    ll ans;
+   while ( low <= high ) {
+       ll mid = low + ( high - low ) / 2;
+       if ( cntProducts(mid, arr) < t ) {
+        low = mid + 1;
+       }
+       else {
+          ans = mid ;
+          high = mid - 1;
+       }
+   }
+      cout << ans << endl;
 }
